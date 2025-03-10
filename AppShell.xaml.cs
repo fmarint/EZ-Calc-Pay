ab3df3192b9ebe451298666ddc7fb722563f50fb
FMarinT <fmarin.devnet@gmail.com>
2025-03-10 14:46:34 -0500

    Agregar archivos de proyecto.

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace EZCalcPay
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace EZCalcPay
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+    }
+
+}
